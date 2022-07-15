ebe42e1faa45468036901a2a7bf9b8058225cf40
Mutaga Kihahu <kihahumutagah@gmail.com>
2022-07-15 11:31:22 +0300

    added states to manipulate the add to cart functionality

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React,{useState} from "react";
 
 function Item({ name, category }) {
+  const [itemInCart , addingToCart] = useState(false)
+  function handlingClick(){
+    addingToCart(()=> !itemInCart )
+    console.log(itemInCart)
+  }
   return (
-    <li className="">
+    <li className={itemInCart? "in-cart" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handlingClick}>{itemInCart? "Remove from Cart":"Add to Cart"}</button>
     </li>
   );
 }
